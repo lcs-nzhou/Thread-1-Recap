b619424977637cc2875ea0a7175ae9b6de42db99
Nina Zhou <nzhou@lcs.on.ca>
2024-01-09 12:01:33 -0500

    Structure of Dog

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,17 @@
 import Cocoa
 
 struct Dog {
+    //MARK: Stored properties
     let breed : String
     let color : String
+    var lengthInMetres : Double
     var heightInMetre : Double
+    
+    //MARK: Computed properties
+    var ratioBetweenLengthAndHeight : Double {
+        return lengthInMetres / heightInMetre
+    }
 }
-var dogOnLeft = Dog(breed: "", color: "brown", heightInMetre: 0.5)
-var dogInMiddle = Dog(breed: "", color: "brown", heightInMetre: 0.6)
+var dogOnLeft = Dog(breed: "", color: "brown",lengthInMetres: 1, heightInMetre: 0.5)
+var dogInMiddle = Dog(breed: "", color: "brown", lengthInMetres: 1, heightInMetre: 0.6)
+dogInMiddle.ratioBetweenLengthAndHeight
