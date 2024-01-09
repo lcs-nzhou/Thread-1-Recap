07417707b7e3e414e1ee8b09a6f3ce4e6d9399c3
Nina Zhou <nzhou@lcs.on.ca>
2024-01-09 11:52:55 -0500

    stucture of dog

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Dog {
+    let breed : String
+    let color : String
+    var heightInMetre : Double
+}
+var dogOnLeft = Dog(breed: "", color: "brown", heightInMetre: 0.5)
+var dogInMiddle = Dog(breed: "", color: "brown", heightInMetre: 0.6)
