f647c810937362ff640548bb4ae52a40ce8f9754
Nina Zhou <nzhou@lcs.on.ca>
2024-01-09 20:04:20 -0500

    second helper view

--- HomeApp/HomeApp/SwiftUIView1.swift
+++ HomeApp/HomeApp/SwiftUIView1.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct SwiftUIView1: View {
+    let imageOnTheLeft : String
+    let firstText : String
+    let secondText : String
+    let thirdText : String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.gray)
+                .frame(width: 150, height: 80)
+            HStack{
+                Image(systemName: imageOnTheLeft)
+                    .foregroundStyle(.white)
+                VStack{
+                    Text(firstText)
+                        .foregroundStyle(.white)
+                    Text(secondText)
+                        .foregroundStyle(.white)
+                    Text(thirdText)
+                        .foregroundStyle(.white)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SwiftUIView1()
+    SwiftUIView1(imageOnTheLeft: "door.garage.double.bay.closed", firstText: "Garage", secondText: "Door", thirdText: "Closed")
 }
